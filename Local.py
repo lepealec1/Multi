c46ca86bd53a4058db5de4eea337dfe33f9de44a
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:05:05 -0700

    Update Local.py

--- Local.py
+++ Local.py
@@ -1,6 +1,8 @@
 import redis
 import streamlit as st
 
+st.warning(1123)
+
 r = redis.Redis(
     host="redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com",
     port=11322,
@@ -10,5 +12,4 @@ r = redis.Redis(
     ssl_cert_reqs=None       # optional (helps avoid cert issues)
 )
 
-st.warning(1)
 r.ping()
