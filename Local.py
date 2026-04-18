718b529415d37e0f9a5748efe4f7e003e82d4657
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:06:07 -0700

    a

--- Local.py
+++ Local.py
@@ -1,15 +1,14 @@
 import redis
 import streamlit as st
-
-st.warning(1123)
+st.write(123)
 
 r = redis.Redis(
     host="redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com",
     port=11322,
     username="default",
-    password=st.secrets["REDIS_PASSWORD"],
-    ssl=True,                # 🔥 REQUIRED
-    ssl_cert_reqs=None       # optional (helps avoid cert issues)
+    password=st.secrets["REDIS_PASSWORD"],  # your long token
+    ssl=True,
+    ssl_cert_reqs=None
 )
 
-r.ping()
+print(r.ping())
