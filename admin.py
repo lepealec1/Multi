e35228c836d31e8e465adaa3f7c60e74d7f657c0
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:43:53 -0700

    a

--- admin.py
+++ admin.py
@@ -23,11 +23,3 @@ def clear_db(r):
         ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "a")
 
         password = st.text_input("Enter admin password", type="password")
-
-        if st.button("🚨 DELETE ALL REDIS DATA"):
-            if password == ADMIN_PASSWORD:
-                r.flushdb()
-                st.success("🔥 All Redis data cleared!")
-                st.rerun()
-            else:
-                st.error("❌ Wrong password")
