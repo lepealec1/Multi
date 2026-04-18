5f7cdf85534d55e416513aa101916aa75d6eaf33
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:56:06 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import redis, uuid, time
 
-def auto_refresh(seconds=5):
-    if "last_refresh" not in st.session_state:
-        st.session_state.last_refresh = time.time()
 
-    if time.time() - st.session_state.last_refresh > seconds:
-        st.session_state.last_refresh = time.time()
+def auto_refresh(seconds=5, key="refresh"):
+    if key not in st.session_state:
+        st.session_state[key] = time.time()
+
+    if time.time() - st.session_state[key] > seconds:
+        st.session_state[key] = time.time()
         st.rerun()
