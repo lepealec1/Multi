bc879cbb3477ac61a7ec50aac7032c4769183721
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:00:10 -0700

    a

--- Local.py
+++ Local.py
@@ -4,7 +4,15 @@ st.write(st.secrets["REDIS_PASSWORD"])
 st.write(int(st.secrets["REDIS_PORT"]))
 st.write(st.secrets["REDIS_HOST"])
 
+import redis
+import streamlit as st
 
+r = redis.from_url(
+    f"rediss://default:{st.secrets['REDIS_PASSWORD']}@redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com:11322",
+    decode_responses=True
+)
+
+r.ping()
 
 r = redis.Redis(
     host="redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com",
