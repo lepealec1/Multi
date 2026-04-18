89da82dad8e7781a8e1f581da9f3025c0f8a5b85
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 19:43:39 -0700

    g

--- Local.py
+++ Local.py
@@ -5,5 +5,18 @@ st.write(int(st.secrets["REDIS_PORT"]))
 st.write(st.secrets["REDIS_HOST"])
 
 
+
+r = redis.Redis(
+    host=st.secrets["REDIS_HOST"],
+    port=int(st.secrets["REDIS_PORT"]),
+    username="default",
+    password=st.secrets["REDIS_PASSWORD"],
+    ssl=True,
+    ssl_cert_reqs=None,
+    decode_responses=True
+)
+
+r.ping()
+
 #r.ping()
 st.success("Redis connected ✅")
