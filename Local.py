ec6780af911051117ee740ec49f96ab1cd8f2762
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:07:32 -0700

    g

--- Local.py
+++ Local.py
@@ -1,14 +1,12 @@
 import redis
 import streamlit as st
-st.write(123)
+st.write(1234)
 
-r = redis.Redis(
-    host="redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com",
-    port=11322,
-    username="default",
-    password=st.secrets["REDIS_PASSWORD"],  # your long token
-    ssl=True,
+
+r = redis.from_url(
+    f"rediss://default:{st.secrets['REDIS_PASSWORD']}@redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com:11322",
     ssl_cert_reqs=None
 )
 
+print(r.ping())
 print(r.ping())
