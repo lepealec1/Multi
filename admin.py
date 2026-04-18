7ae8f6f1b0529a37b91282ce9cd72a873b3a9dec
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:45:46 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,22 +1,17 @@
 import streamlit as st
 import redis, uuid, time
 
-st.subheader("🧹 Admin Tools")
 
 #ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "admin123")
 def clear_db(r):
-    display_name = st.session_state.name
-    if display_name == "alepe":
+    ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "a")
 
-        st.subheader("🧹 Admin Tools")
-        ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "a")
+    password = st.text_input("Enter admin password", type="password")
 
-        password = st.text_input("Enter admin password", type="password")
-
-        if st.button("🚨 DELETE ALL REDIS DATA"):
-            if password == ADMIN_PASSWORD:
-                r.flushdb()
-                st.success("🔥 All Redis data cleared!")
-                st.rerun()
-            else:
-                st.error("❌ Wrong password")
+    if st.button("🚨 DELETE ALL REDIS DATA"):
+        if password == ADMIN_PASSWORD:
+            r.flushdb()
+            st.success("🔥 All Redis data cleared!")
+            st.rerun()
+        else:
+            st.error("❌ Wrong password")
