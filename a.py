ec471c1b1fd843b06619577fe6b905822fdf5e0a
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:17:28 -0700

    a

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
-import redis
-import streamlit as st
-st.write(1234)
+"""Basic connection example.
+"""
 
+import redis
 
 r = redis.Redis(
     host='redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com',
@@ -10,6 +10,7 @@ r = redis.Redis(
     username="default",
     password="UxXQyBngaO7mDWWesUdn7WPsDcBqRYdv",
 )
+
 success = r.set('foo', 'bar')
 # True
 
