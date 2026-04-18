5a5cb74773fa7a420e8cbb248d237f355ea3c4a9
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:42:55 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -5,14 +5,29 @@ st.subheader("🧹 Admin Tools")
 
 #ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "admin123")
 def clear_db(r):
-    ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "a")
+    display_name = st.session_state.name
+    if display_name == "alepe":
 
-    password = st.text_input("Enter admin password", type="password")
+        st.subheader("🧹 Admin Tools")
 
-    if st.button("🚨 DELETE ALL REDIS DATA"):
-        if password == ADMIN_PASSWORD:
-            r.flushdb()
-            st.success("🔥 All Redis data cleared!")
-            st.rerun()
-        else:
-            st.error("❌ Wrong password")
+        password = st.text_input("Enter admin password", type="password")
+        confirm = st.checkbox("I understand this will delete EVERYTHING")
+
+        if st.button("🚨 DELETE ALL REDIS DATA"):
+            if password == st.secrets["ADMIN_PASSWORD"] and confirm:
+                r.flushdb()
+                st.success("🔥 All Redis data cleared!")
+                st.rerun()
+            else:
+                st.error("❌ Wrong password or missing confirmation")
+        ADMIN_PASSWORD = st.secrets.get("ADMIN_PASSWORD", "a")
+
+        password = st.text_input("Enter admin password", type="password")
+
+        if st.button("🚨 DELETE ALL REDIS DATA"):
+            if password == ADMIN_PASSWORD:
+                r.flushdb()
+                st.success("🔥 All Redis data cleared!")
+                st.rerun()
+            else:
+                st.error("❌ Wrong password")
