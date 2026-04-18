6eb4db169e3e73d5d8f98996fba5738401a07b12
lepealec2 <117782443+lepealec2@users.noreply.github.com>
2026-04-17 21:03:32 -0700

    Update Local.py

--- Local.py
+++ Local.py
@@ -1,11 +1,8 @@
-import redis
-import ssl
-import streamlit as st
+import redis, ssl
 
 r = redis.from_url(
-    f"rediss://default:{st.secrets['REDIS_PASSWORD']}@redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com:11322",
-    ssl_cert_reqs=ssl.CERT_NONE,   # key fix
-    decode_responses=True
+    "rediss://default:YOUR_PASSWORD@redis-11322.c12.us-east-1-4.ec2.cloud.redislabs.com:11322",
+    ssl_cert_reqs=ssl.CERT_NONE
 )
 
 print(r.ping())
